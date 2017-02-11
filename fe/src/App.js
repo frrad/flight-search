b660ecbd408e3e801f5f582423651269babebbb5
zac <zac.cucinotta@gmail.com>
2017-02-10 23:31:06 -0800

    merge

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SearchForm from './searchform.js';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Flight Search</h2>
         </div>
         <SearchForm />
--- fe/src/searchform.js
+++ fe/src/searchform.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import ConditionEntry from './searchform.js';
 
-export default class SearchForm extends Component {
+export default class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', conditions: ['one']};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('A thing was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  addCondition(event) {
+    console.log('pushed the button: ' + this.state.value);
+    event.preventDefault();
+    var condition = <button />;
+
+  }
+
   render() {
+    let conditions = []
+    this.state.conditions.forEach(function(con) {
+        console.log(con)
+        conditions.push(<ConditionEntry />)
+
+    })
     return (
-      <div className="search form">
-        <p className="App-intro">
-          there should be a form here
-        </p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <div className="conditionEntry">
+          {conditions}
+          <label>
+            Condition:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+        </div>
+        <button onClick="{addCondition()}">
+          Add Condition
+        </button>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
