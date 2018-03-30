f9f901e4a91c18735c01ff9f5ade05d8eb13c9f9
Frederick Robinson <frederick.robinson@gmail.com>
2018-03-29 21:36:59 -0700

    works if barely

--- flight-backend/backend.go
+++ flight-backend/backend.go
@@ -9,15 +9,13 @@ import (
 
 	"github.com/frrad/flight-search/flight-backend/qpx"
 	"github.com/frrad/flight-search/flight-backend/querydag"
-	"github.com/frrad/flight-search/flight-backend/querytree"
 	"github.com/frrad/flight-search/flight-backend/trip"
 )
 
-func test(rw http.ResponseWriter, req *http.Request) {
+func queryHandler(w http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
 
-	var t querytree.Tree
-
+	var t querydag.DAG
 	err := decoder.Decode(&t)
 
 	if err != nil {
@@ -29,88 +27,23 @@ func test(rw http.ResponseWriter, req *http.Request) {
 
 	defer req.Body.Close()
 
-	log.Println("\n" + t.DispFormat(0))
-	log.Println(len(t.Reduce()))
-	// for _, res := range t.Resolve() {
-	// 	log.Println("asdfasfd")
-	// 	log.Println("\n" + res.DispFormat(0))
-	// }
-	log.Println("test")
+	finder := qpx.NewQPXFinder(os.Getenv("QPXAPIKEY"))
+	planner := trip.NewPlanner(finder)
 
+	sols := t.AllSolutions()
+	options := planner.ListOptions(sols)
+	response, err := json.Marshal(options)
+
+	if err == nil {
+		fmt.Fprintf(w, "%s", string(response))
+	} else {
+		fmt.Fprintf(w, "ERROR")
+	}
 }
 
 func main() {
 
-	// http.HandleFunc("/backend", test)
-	// http.ListenAndServe(":8080", nil)
-
-	test := querydag.DAG{
-		Nodes: []querydag.Node{
-			{
-				Name: "start",
-				FlightsOut: []querydag.Flight{
-					{ToNode: 1},
-					{ToNode: 2},
-				},
-			},
-			{
-				Name:      "SFO",
-				IsAirport: true,
-				FlightsOut: []querydag.Flight{
-					{
-						ToNode: 3,
-						Dates:  []string{"2018-04-01"},
-					},
-				},
-			},
-			{
-				Name:      "OAK",
-				IsAirport: true,
-				FlightsOut: []querydag.Flight{
-					{
-						ToNode: 3,
-						Dates:  []string{"2018-04-01", "2018-04-02"},
-					},
-				},
-			},
-			{
-				Name:      "<>",
-				IsAirport: false,
-				FlightsOut: []querydag.Flight{
-					{ToNode: 4},
-					{ToNode: 5},
-				},
-			},
-			{
-				Name:      "MCO",
-				IsAirport: true,
-				FlightsOut: []querydag.Flight{
-					{
-						ToNode: 6,
-					},
-				},
-			},
-
-			{
-				Name:      "MIA",
-				IsAirport: true,
-				FlightsOut: []querydag.Flight{
-					{
-						ToNode: 6,
-					},
-				},
-			},
-
-			{Name: "end"},
-		},
-	}
-
-	fmt.Println(test.Viz())
-
-	finder := qpx.NewQPXFinder(os.Getenv("QPXAPIKEY"))
-	planner := trip.NewPlanner(finder)
-
-	sols := test.AllSolutions()
-	fmt.Println(planner.ListOptions(sols))
+	http.HandleFunc("/backend", queryHandler)
+	http.ListenAndServe(":8080", nil)
 
 }
